3023acaa44eeb7e796527af0b138a7373b63af90
ajeyanadig <ajeyanadig@gmail.com>
2024-03-26 14:06:28 +0530

    Grammar error fix

--- src/Components/StartScreen.js
+++ src/Components/StartScreen.js
@@ -2,7 +2,7 @@ function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome To The React Quiz</h2>
-      <h3>{numQuestions} questions to test your react master</h3>
+      <h3>{numQuestions} questions to test your react mastery</h3>
       <button className="btn" onClick={() => dispatch({ type: "start" })}>
         Let's start
       </button>
